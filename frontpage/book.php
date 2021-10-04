10221d39910f2fc4fc3035094f39f9822062f3a5
ryanfigo <ryanfigo12@gmail.com>
2021-10-04 20:30:08 +0800

    Web Figo

---
+++ frontpage/book.php
@@ -0,0 +1,55 @@
+<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-black-100 font-bold leading-tight text-center md:text-left slide-in-bottom-h1 text-indigo-50">Kue Bali</h1>
+		</div>
+<div class="grid grid-cols-2 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/kue lukis.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kue Lukis</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pesan</button></a>
+					<p class="mt-2 text-gray-500">Kue lukis ini terbuat dari ketan dan parutan kelapa serta gula merah, kue bali ini terkenal karna manis dan bentuknya segitiga dan sangat unik.</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/kue injin.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kue Injin</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pesan</button></a>
+						<p class="mt-2 text-gray-500">Kue Injin ini terbuat dari campuran ketan hitam dan ketan putih dan parutan kelapa serta gula merah, kue bali ini terkenal manis dan warnanya hitam alami dari ketannya.</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/godoh.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Pisang goreng</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pesan</button></a>
+						<p class="mt-2 text-gray-500">Pisang goreng ini terbuat dari campuran tepung dan buah pisang serta dipadukan dengan gula merah.</p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/laklak.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kue Laklak</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pesan</button></a>
+						<p class="mt-2 text-gray-500">Kue laklak ini terbuat dari campuran tepung beras dan tepung ketan serta air daun suji serta dipadukan dengan parutan kelapa dan gula merah.</p>
+					</div>
+					</div>
+			</div>
+		</div>
+
+		</div>
---
+++ frontpage/rekap.php
@@ -0,0 +1,40 @@
+<div class="items-center bg-purple-700 rounded-xl shadow-md overflow-hidden md:max-w-full slide-in-bottom">
+        <div class="px-8 mb-10 text-center py-12">
+          <h2 class="px-4 py-3 border text-center font-bold">Pemesanan</h2>
+            <div class="mt-8 max-w-md">
+            <div class="grid grid-cols-1 gap-6">
+              <label class="block">
+                <span class="text-black">Nama</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-black">No HP</span>
+                <input type="number" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-black">Alamat</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-black">Tanggal Pesanan</span>
+                <input type="Date" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-black">Jenis Kue Bali</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                  <option>Kue Lukis</option>
+                  <option>Kue Laklak</option>
+                  <option>Pisang Goreng</option>
+                  <option>Kue Injin</option>
+                </select>
+              </label>
+              <label class="block">
+                <span class="text-black">Keterangan pada penjual</span>
+                <textarea
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" rows="3"
+                ></textarea>
+              </label>
+            </div>
+            </div>
+        </div>
+</div>
